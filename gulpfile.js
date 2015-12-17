104e7dbb3d336cb7f292368c08c4e2bd34cd18c4
Sadeq <sadeq.sardari@gmail.com>
2015-12-17 03:59:00 +0330

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,26 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
 	concat = require('gulp-concat');
-	
-var coffeeSources = ['components/coffee/tagline.coffee'];
 
-gulp.task('coffee', function() {
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'];
+
+gulp.task('coffee', function () {
 	gulp.src(coffeeSources)
-		.pipe(coffee({bare: true})
+		.pipe(coffee({ bare: true })
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
-		
+
 })
 
-gulp.task('concat', function() {
-	gulp.src('components/scripts/*.js')
+gulp.task('concat', function () {
+	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
-
-
-
-
-
-
-
-
-
-
-
